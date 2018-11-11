a5675575f31984484e58b9091ea820cab69f1245
kipitix <kipitix@gmail.com>
2018-11-11 22:42:41 +0300

    memmove

--- SerialUnitBuffer.cpp
+++ SerialUnitBuffer.cpp
@@ -1,44 +1,73 @@
 #include "SerialUnitBuffer.h"
 
 
-SerialUnitBuffer::SerialUnitBuffer(size_t maxSize)
-	: _maxSize(maxSize)
-	, _size(0)
+SerialUnitBuffer::SerialUnitBuffer(size_t size,
+								   const char * unitStart,
+								   const char * unitEnd)
+	: _size(size)
 	, _buffer(0)
+	, _unit(0)
+	, _unitStart(unitStart)
+	, _unitEnd(unitEnd)
 {
-	if (_maxSize > 0)
+	if (_size > 0)
 	{
-		_buffer = new char[_maxSize + 1]; // +1 for zero char
-		_buffer[0] = 0;
+		_buffer = new char[_size + 1]; // +1 for zero char
+		_buffer[0] = 0; // End of string
 	}
 }
 
 
 SerialUnitBuffer::~SerialUnitBuffer()
 {
-	if (_maxSize > 0)
+	if (_buffer != 0)
 	{
 		delete [] _buffer;
+	}
+
+	if (_unit != 0)
+	{
+		delete [] _unit;
 	}
 }
 
 
-bool SerialUnitBuffer::append(char c)
+void SerialUnitBuffer::append(char c)
 {
+	char * tmp = new char[2]; // char and zero end
+	tmp[0] = c;
+	tmp[1] = 0;
+	append(tmp);
+	delete [] tmp;
+}
+
+
+void SerialUnitBuffer::append(const char * str)
+{
+	if (_buffer == 0)
+	{
+		return;
+	}
+
+	size_t currentSize = strlen(_buffer);
+	size_t strSize = strlen(str);
+	if (strSize > _size)
+	{
+		strSize = _size;
+	}
+
+	if (currentSize >= _size)
+	{
+		memmove(_buffer, _buffer + strSize, _size - strSize + 1);
+	}
+
 	if (_size < _maxSize)
 	{
 		_buffer[_size] = c;
 		_size ++;
 		_buffer[_size] = 0;
-		return true;
 	}
 
-	return false;
-}
-
-
-bool SerialUnitBuffer::append(const char * str)
-{
 	size_t appSize = strlen(str);
 	if (_size + appSize <= _maxSize)
 	{
--- SerialUnitBuffer.h
+++ SerialUnitBuffer.h
@@ -6,24 +6,27 @@
 class SerialUnitBuffer
 {
 public:
-	SerialUnitBuffer(size_t maxSize = 1024,
+	SerialUnitBuffer(size_t size = 1024,
 					 const char * unitStart = ">>>",
 					 const char * unitEnd = "<<<");
 	virtual ~SerialUnitBuffer();
 
 
-	size_t maxSize() const { return _maxSize; }
 	size_t size() const { return _size; }
-	const char * buffer() const { return _buffer; }
+	char * buffer() const { return _buffer; }
 
-	bool append(char c);
-	bool append(const char * str);
+	void append(char c);
+	void append(const char * str);
 
+	bool nextUnitExist() const;
+	char * goToNextUnit();
+
+	char * nextUnit() const { return _unit; }
 
 private:
-	size_t _maxSize;
 	size_t _size;
 	char * _buffer;
+	char * _unit;
 
 	const char * _unitStart;
 	const char * _unitEnd;
